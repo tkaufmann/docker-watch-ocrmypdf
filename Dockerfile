da43ad8ccc985bb3a3eb5aea620ca5c75ac44b37
Tim Kaufmann <tim@timkaufmann.de>
2022-07-24 01:14:02 +0200

    jo

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM jbarlow83/ocrmypdf:latest
 
 RUN apt-get update && \
-    apt-get install inotify-tools curl unzip man-db -y && \
+    apt-get install inotify-tools curl unzip less nano man-db -y && \
     apt-get clean  && rm -rf /var/lib/apt/lists/* /var/tmp/* /tmp/*
 
 RUN curl https://rclone.org/install.sh | bash
--- watch.sh
+++ watch.sh
@@ -70,10 +70,13 @@ inotifywait -r -m $WOCR_CONSUME_PATH -e create -e moved_to |
         fi
 
         if [ ! -z "${!cmdVarName}" ]; then
+            # after_cmd=$(echo ${!cmdVarName} | sed "s|%FULLFILE%|$fullfile|" | sed "s|%FILENAME%|$filename.pdf|")
+            # echo $after_cmd
+            # eval "$after_cmd";
+
             after_cmd=($(echo ${!cmdVarName} | sed "s|%FULLFILE%|$fullfile|" | sed "s|%FILENAME%|$filename.pdf|"))
-           
-            echo $after_cmd
-            eval "$after_cmd";
+            echo ${after_cmd[@]}
+            "${after_cmd[@]}"
         else
             echo no WOCR_AFTER command set.
         fi
